05a0955b5df9cfe30e12da23302d79e4f55b79dc
berendeanicolae <berendeanicolae@gmail.com>
2014-08-03 19:42:22 +0300

    add fast reading

--- SPOJ/MULTQ3/main.cpp
+++ SPOJ/MULTQ3/main.cpp
@@ -8,7 +8,7 @@ struct node{
     int add;
 };
 
-int N, Q;
+int N, Q, li, ls;
 node tree[SIZE];
 
 void read();
@@ -24,23 +24,38 @@ int main()
 void init(int pos, int li, int ls){
     tree[pos].value[0] = ls-li+1;
     if (li<ls){
-        init(pos*2+1, li, (li+ls)/2);
-        init(pos*2+2, (li+ls)/2+1, ls);
+        init((pos<<1)+1, li, (li+ls)/2);
+        init((pos<<1)+2, (li+ls)/2+1, ls);
     }
 }
 
+int readint() {
+    int n = 0;
+    char c = getchar_unlocked();
+    while ( !( '0' <= c && c <= '9' ) ) {
+        c = getchar_unlocked();
+    }
+    while ( '0' <= c && c <= '9' ) {
+        n = n * 10 + c - '0';
+        c = getchar_unlocked();
+    }
+    return n;
+}
+
 void read(){
-    freopen("txt.in", "r", stdin);
+    //freopen("txt.in", "r", stdin);
     //freopen("txt.out", "w", stdout);
-    scanf("%d %d", &N, &Q);
+    N = readint();
+    Q = readint();
+    //scanf("%d %d", &N, &Q);
     init(0, 0, N-1);
 }
 
 void push(int pos){
     tree[pos].add %= 3;
     if (tree[pos].value[0]+tree[pos].value[1]+tree[pos].value[2]  > 1){
-        tree[pos*2+1].add += tree[pos].add;
-		tree[pos*2+2].add += tree[pos].add ;
+        tree[(pos<<1)+1].add += tree[pos].add;
+		tree[(pos<<1)+2].add += tree[pos].add;
     }
     switch (tree[pos].add){
         case 1:
@@ -56,71 +71,74 @@ void push(int pos){
 }
 
 void pull(int pos){
-    tree[pos].value[0] = tree[pos*2+1].value[0]+tree[pos*2+2].value[0];
-    tree[pos].value[1] = tree[pos*2+1].value[1]+tree[pos*2+2].value[1];
-    tree[pos].value[2] = tree[pos*2+1].value[2]+tree[pos*2+2].value[2];
+    tree[pos].value[0] = tree[(pos<<1)+1].value[0]+tree[(pos<<1)+2].value[0];
+    tree[pos].value[1] = tree[(pos<<1)+1].value[1]+tree[(pos<<1)+2].value[1];
+    tree[pos].value[2] = tree[(pos<<1)+1].value[2]+tree[(pos<<1)+2].value[2];
 }
 
-void update(int li, int ls, int pos, int l, int r){
-    push(pos);
+void update(int pos, int l, int r){
     if (li<=l && r<=ls){
         ++tree[pos].add;
         push(pos);
         return;
     }
 
+    push(pos);
     int m = (l+r)/2;
     if (m >= ls){
-        update(li, ls, pos*2+1, l, m);
+        update((pos<<1)+1, l, m);
 		if (l<r){
-			push(pos*2+1);
+			push((pos<<1)+2);
 		}
         pull(pos);
         return;
     }
     if (m < li){
-        update(li, ls, pos*2+2, m+1, r);
+        update((pos<<1)+2, m+1, r);
 		if (l<r){
-			push(pos*2+2);
+			push((pos<<1)+1);
 		}
         pull(pos);
         return;
     }
 
-    update(li, ls, pos*2+1, l, m);
-    update(li, ls, pos*2+2, m+1, r);
+    update((pos<<1)+1, l, m);
+    update((pos<<1)+2, m+1, r);
 	if (l<r){
-		push(pos*2+1);
-		push(pos*2+2);
+		push((pos<<1)+1);
+		push((pos<<1)+2);
 	}
     pull(pos);
 }
 
-int query(int li, int ls, int pos, int l, int r){
-	push(pos);
+int query(int pos, int l, int r){
+    push(pos);
     if (li<=l && r<=ls){
         return tree[pos].value[0];
     }
 
     int m = (l+r)/2;
     if (m >= ls)
-        return query(li, ls, pos*2+1, l, m);
+        return query((pos<<1)+1, l, m);
     if (m < li)
-        return query(li, ls, pos*2+2, m+1, r);
-    return query(li, ls, pos*2+1, l, m) + query(li, ls, pos*2+2, m+1, r);
+        return query((pos<<1)+2, m+1, r);
+    return query((pos<<1)+1, l, m) + query((pos<<1)+2, m+1, r);
 }
 
 void solve(){
     for (int q=0; q<Q; ++q){
-        int op, a, b;
+        int op;
 
-        scanf("%d %d %d", &op, &a, &b);
+        op = readint();
+        li = readint();
+        ls = readint();
+        //scanf("%d %d %d", &op, &li, &ls);
         switch (op){
             case 0:
-                update(a, b, 0, 0, N-1);
+                update(0, 0, N-1);
                 break;
             case 1:
-                printf("%d\n", query(a, b, 0, 0, N-1));
+                printf("%d\n", query(0, 0, N-1));
                 break;
         }
     }
